168e98389b622f720784001b54259604a00d0073
fudiwara <fudiwara@users.noreply.github.com>
2020-05-20 14:06:29 +0900

    Add files via upload

---
+++ s04_capture2image/cam2img.cpp
@@ -0,0 +1,35 @@
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char *argv[]){
+	int key, imageNum = 0;
+	cv::Mat frame;
+	char saveFileName[256];
+	
+	// カメラ関連
+	cv::VideoCapture cap(0);
+	cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+	cap.set(cv::CAP_PROP_FPS, 30);
+	
+	// カメラがオープンできたかの確認
+	if(!cap.isOpened()) return -1;
+	
+	while(1){
+		cap >> frame;  // キャプチャ
+		cv::imshow("capture", frame);
+		// 様々な処理
+		// ...
+		key = cv::waitKey(1);
+		if(key == 'q' || key == 27){
+			break;
+		}else if(key == 's'){
+			sprintf(saveFileName, "%03d.jpg", imageNum);
+			cv::imwrite(saveFileName, frame);
+			imageNum++;
+		}
+	}
+	
+	cv::destroyAllWindows();
+	cap.release();
+	return 0;
+}
